491947ad362a29e0d35859c178f18fdd8b6c3be6
sarathraj10 <sarath.raj@spericorn.com>
2021-10-27 10:08:13 +0530

    fix

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -2,9 +2,9 @@ import axios from 'axios'
 
 const API = axios.create({baseURL : 'http://localhost:5000/user'})
 API.interceptors.request.use((req)=>{
-    const profile = localStorage.getItem('profile')
-    if(profile){
-        req.headers.Authorization = `Bearer ${JSON.parse(profile).token}`
+    const token = localStorage.getItem('token')
+    if(token){
+        req.headers.Authorization = `Bearer ${token}`
     }
     return req
 })
